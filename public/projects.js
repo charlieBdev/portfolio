cf073bc21cc4bbb7e602fd2bd4d413437917975b
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-08-22 17:11:44 +0100

    about page

---
+++ public/projects.js
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+        "name": "SoundWaves",
+        "desc": "A more accessible Twitter experience.",
+        "imgURL": "",
+        "keywords": ["AssemblyAI", "Next.js", "Supabase", "TypeScript", "Northcoders"],
+        "btnText": "Coming soon...",
+        "btnLink": ""
+    },
+    {
+        "name": "NC News",
+        "desc": "A stipped-back Reddit clone.",
+        "imgURL": "",
+        "keywords": ["Express.js", "Jest", "MVC", "Postgres", "React", "RESTful API", "Supertest", "TDD", "Vanilla CSS", "Northcoders"],
+        "btnText": "Go to NC News",
+        "btnLink": "https://euphonious-scone-36cbae.netlify.app/"
+    },
+    {
+        "name": "Meteorites App",
+        "desc": "Use the NASA Meteroite Landings API to get data on Meteorites in a given year.",
+        "imgURL": "",
+        "keywords": ["Axios", "Express.js", "Pagination", "React", "RESTful API", "TDD", "Northcodes"],
+        "btnText": "Check out space stuff",
+        "btnLink": "https://precious-madeleine-f3b23d.netlify.app/"
+    },
+    {
+        "name": "myDictionary",
+        "desc": "To help English language learners remember vocabulary in a personalised way.",
+        "imgURL": "",
+        "keywords": ["Bootstrap", "Flask", "Python", "SQLite3", "CS50"],
+        "btnText": "Watch a demo",
+        "btnLink": "https://www.youtube.com/embed/SKNA0gKNQ8o"
+    },
+    {
+        "name": "Sunny Surfboards",
+        "desc": "A calculator of sorts",
+        "imgURL": "",
+        "keywords": ["C#", "WinForms", ".NET", "Sheffield College"],
+        "btnText": "View the repo",
+        "btnLink": "https://github.com/charlieBdev/Sunny-Surfboards"
+    }
+]
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,60 @@
+import Image from "next/image"
+import about from "../../../public/about.png"
+
 export default function About() {
     return (
-      <main className="flex flex-col h-screen justify-center overflow-hidden">
-
+      <main className="flex flex-col h-screen justify-between overflow-hidden">
+        <div className="p-3 text-sm lg:text-center">
+          <p className="mb-1">Hey, I have just finished a bootcamp with <span className="text-neutral-400">Northcoders</span> and am looking for <span className="text-neutral-400">junior</span> or entry-level roles in <span className="text-neutral-400">Sheffield</span> or remote.</p>
+          <p>Check out my <span className="text-neutral-400">projects</span> or send a message via the <span className="text-neutral-400">contact</span> page if you'd like a chat.</p>
+        </div>
+        <div className="flex flex-row justify-around lg:justify-evenly ">
+          <div className="bg-neutral-900 sm:rounded-md p-3 w-32 text-sm">
+            <p className="text-green-500">Frontend</p>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Tailwind</li>
+              <li>React</li>
+              <li>Next.js</li>
+            </ul>
+          </div>
+          <div className="bg-neutral-900 sm:rounded-md p-3 w-32 text-sm">
+            <p className="text-green-500">Backend</p>
+            <ul>
+              <li>Express</li>
+              <li>Postgres</li>
+              <li>Axios</li>
+              <li className="text-neutral-400">Flask</li>
+            </ul>
+          </div>
+          <div className="bg-neutral-900 sm:rounded-md p-3 w-32 text-sm">
+            <p className="text-green-500">Languages</p>
+            <ul>
+              <li>JavaScript</li>
+              <li className="text-neutral-400">Python</li>
+              <li className="text-neutral-400">C#</li>
+            </ul>
+          </div>
+          <div className="bg-neutral-900 sm:rounded-md p-3 w-32 text-sm">
+            <p className="text-green-500">Qualities</p>
+            <ul>
+              <li>Great team player</li>
+              <li>Attention to detail</li>
+              <li>Goes the extra mile</li>
+            </ul>
+          </div>
+        </div>  
+        <Image
+          src={about}
+          // width={500}
+          // height={500}
+          alt="Lots of things Charlie likes: coding, games, space, coffee, music, nature, peace, and love."
+          className="max-h-full object-scale-down mx-auto"
+          priority={true}
+          placeholder = 'blur'
+          height='500'
+        />
       </main>
     )
 }
