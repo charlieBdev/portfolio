2d9a283ca86e22ee2bfece233b4c2aeadd12837e
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-08-26 18:06:45 +0100

    check providers

--- src/app/Components/Nav.tsx
+++ src/app/Components/Nav.tsx
@@ -15,10 +15,10 @@ export default function Nav() {
     <div 
       className="p-6 flex items-center justify-between border-b border-neutral-500 sticky top-0 z-50"
     >
-      <div>
+      <Link href="/">
         <h1 className="text-3xl font-bold text-cyan-400">CHARLIE BISHOP</h1>
         <p className="text-lg text-emerald-400">Junior<span className="text-yellow-300"> Full Stack Web Developer</span></p>
-      </div>
+      </Link>
       <nav className="flex space-x-3 lg:space-x-12">
         <ThemeChanger />
         <section className="MOBILE-MENU flex lg:hidden">
