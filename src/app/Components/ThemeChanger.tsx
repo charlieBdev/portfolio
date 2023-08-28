490019842e8c1678346b6e1d7d32c9e1a2b4de8f
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-08-28 20:41:51 +0100

    more import fixes

--- src/app/Components/ThemeChanger.tsx
+++ src/app/Components/ThemeChanger.tsx
@@ -4,7 +4,7 @@ import Dark from "./Dark"
 
 export default function ThemeChanger() {
     const { theme, setTheme } = useTheme()
-
+console.log(theme, '<<< theme')
     return (
         <div className="space-x-3">
             <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>{theme === 'light' ? <Light /> : <Dark />}</button>
