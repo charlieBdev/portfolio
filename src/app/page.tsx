1d83dbb62f8e87f7bf6ebc896f651b2ce104b7ab
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-09-01 09:42:36 +0100

    add blinking repeat with react-spring

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,19 @@
 "use client"
 
 import Link from "next/link"
+import Spring from "./Components/Spring"
 
 // import React from 'react'
 // import { useTheme } from "next-themes";
 
 
 export default function Home() {
-
   return (
-      <section className="flex flex-col justify-center items-center min-h-screen">
-        <p className="text-6xl animate-pulse text-center">e<Link href="/about" className="text-purple-300 animate-pulse">a</Link>t</p>
-        <p className="text-6xl animate-pulse text-center">sleep</p>
-        <p className="text-6xl animate-pulse text-center"><Link href="/contact" className="text-purple-300 animate-pulse">c</Link>ode</p>
-        <p className="text-6xl animate-pulse text-center">re<Link href="/projects" className="text-purple-300 animate-pulse">p</Link>eat</p>
-      </section>
+    <section className="flex flex-col justify-center items-center min-h-screen">
+      <p className="text-6xl text-center">e<Link href="/about" className="text-purple-300">a</Link>t</p>
+      <p className="text-6xl text-center">slee<Link href="/projects" className="text-purple-300">p</Link></p>
+      <p className="text-6xl text-center"><Link href="/contact" className="text-purple-300">c</Link>ode</p>
+      <p className="text-6xl text-center flex">{Spring("repeat")}</p>
+    </section>
   )
 }
