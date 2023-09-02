90dc733172e61b9684ff1da7b5333be930bd49b5
bigspambot <67501435+bigspambot@users.noreply.github.com>
2023-09-02 18:35:57 +0100

    get random song on each click

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,33 @@
 import Link from "next/link"
 import Flunderscore from "./Components/Flunderscore"
 import getRandomSong from "@/utils/getRandomSong"
+import { useEffect, useState } from "react"
 
 
 export default function Home() {
+
+  interface Window {
+    open: (url: string, target: string) => void;
+  }
+
+  const [songURL, setSongURL] = useState<string>('')
+
+  useEffect(() => {
+    setSongURL(getRandomSong())
+  }, [])
+
+  const handleClick = () => {
+    const newSongURL = getRandomSong();
+    setSongURL(newSongURL);
+    window.open(songURL, '_blank');
+  }  
 
   return (
     <section className="flex flex-col justify-center items-center min-h-screen">
       <p className="text-6xl text-center">e<Link href="/about" className="text-purple-300">a</Link>t</p>
       <p className="text-6xl text-center">slee<Link href="/projects" className="text-purple-300">p</Link></p>
       <p className="text-6xl text-center"><Link href="/contact" className="text-purple-300">c</Link>ode</p>
-      <p className="text-6xl text-center">rep<Link href={getRandomSong()} target="_blank" className="text-purple-300">e</Link>at</p>
+      <p className="text-6xl text-center">rep<Link onClick={handleClick} href="/" className="text-purple-300">e</Link>at</p>
       <Flunderscore />
     </section>
   )
--- src/utils/getRandomSong.tsx
+++ src/utils/getRandomSong.tsx
@@ -1,14 +1,9 @@
 export default function getRandomSong() {
     const songs = [
-        "https://www.youtube.com/watch?v=Sfz9jQ3KI5k",
         "https://www.youtube.com/watch?v=S8gymPzmtZs",
         "https://www.youtube.com/watch?v=sgyNTvovQTg",
-        "https://www.youtube.com/watch?v=VKF5LCHZREg",
         "https://www.youtube.com/watch?v=NOG3eus4ZSo",
         "https://www.youtube.com/watch?v=SH4RnGNBNxo",
-        "https://www.youtube.com/watch?v=Ds0jtuyslbI",
-        "https://www.youtube.com/watch?v=kBOaLjtR4mw",
-        "https://www.youtube.com/watch?v=BI2Et19vDCM"
     ]
     const randomNum = Math.round(Math.random() * songs.length)
     return songs[randomNum]
