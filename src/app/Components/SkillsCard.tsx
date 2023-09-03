f03f305d6d4ea700633432830469f02fe899bc6c
charlieBdev <chazbish@hotmail.com>
2023-09-03 16:37:38 +0100

    add skeletons and skills icons

--- src/app/Components/SkillsCard.tsx
+++ src/app/Components/SkillsCard.tsx
@@ -1,12 +1,18 @@
 interface SkillsCardProps {
-  topic: string;
-  skills: string[];
+  topic: string
+  skills: string[]
+  svg: JSX.Element
 }
 
-const SkillsCard: React.FC<SkillsCardProps> = ({ topic, skills }) => {
+const SkillsCard: React.FC<SkillsCardProps> = ({ topic, skills, svg }) => {
   return (
     <div className="rounded-lg p-3 pt-2 w-full h-44 text-sm shadow dark:border dark:border-neutral-800">
-        <p className="text-center text-lg text-green-500 p-1">{topic}</p>
+        <div className="flex flex-row items-center justify-center text-green-500">
+          {svg}
+          <p className="text-lg text-green-500 p-1">
+            {topic}
+          </p>
+        </div>
         <ul className="text-md space-y-1 text-center">
             {skills.map(skill => (
               <li key={skill} className="">{skill}</li>
--- src/app/Components/SkillsList.tsx
+++ src/app/Components/SkillsList.tsx
@@ -1,13 +1,37 @@
+"use client"
+
 import skillsArr from "@/data/skillsArr"
 import SkillsCard from "./SkillsCard"
+import SkillsCardSkeleton from "./SkillsCardSkeleton"
+import { useEffect, useState } from "react"
 
 const SkillsList = () => {
+
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 1000)
+  })
+
   return (
     // <div className="flex flex-col sm:grid sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
     <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-3">
-        {skillsArr.map(skill => (
-            <SkillsCard key={skill.topic} topic={skill.topic} skills={skill.skills}/>
-        ))}
+        {isLoading ? (
+          <>
+            <SkillsCardSkeleton />
+            <SkillsCardSkeleton />
+            <SkillsCardSkeleton />
+            <SkillsCardSkeleton />
+            <SkillsCardSkeleton />
+            <SkillsCardSkeleton />
+          </>
+        ) : (
+          skillsArr.map(skill => (
+            <SkillsCard key={skill.topic} topic={skill.topic} skills={skill.skills} svg={skill.svg} />
+          ))
+        )}
     </div>
   )
 }
