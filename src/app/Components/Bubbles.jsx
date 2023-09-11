26238805119f51dadc4a53bf1e4e21a01fa73ce6
charlieBdev <chazbish@hotmail.com>
2023-09-11 11:39:05 +0100

    add bubbles

--- src/app/Components/Bubbles.jsx
+++ src/app/Components/Bubbles.jsx
@@ -1,39 +1,57 @@
 import React, { useRef, useEffect } from "react";
 
 const Bubbles = () => {
-	const canvasRef = useRef(null);
-	const particleArray = [];
+  const canvasRef = useRef(null);
+  const particleArray = [];
+  const particlesToRemove = [];
 
-	useEffect(() => {
-		const canvas = canvasRef.current;
-		const context = canvas.getContext("2d");
-		canvas.width = window.innerWidth;
-		canvas.height = window.innerHeight;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-		const Particle = class {
+    class Particle {
 			constructor(x = 0, y = 0) {
 				this.x = x;
 				this.y = y;
 				this.radius = Math.random() * 50;
-				this.dx = Math.random() * 3;
-				this.dy = Math.random() * 7;
-				this.color = ""; // Initialize as an empty string
+				this.dx = (Math.random() - 0.5) * 3; // Random horizontal velocity between -1.5 and 1.5
+				this.dy = Math.random() * 3; // Random vertical velocity upwards
+				this.colors = [
+					"244, 114, 182",
+					"216, 180, 254",
+					"34, 211, 238",
+					"52, 211, 153",
+					"253, 224, 71",
+					"34, 197, 94"
+				];
+				this.color = this.getRandomColor();
+				this.creationTime = Date.now();
 			}
-
-			setColorBasedOnBackground() {
-				const canvasBackgroundColor =
-					window.getComputedStyle(canvas).backgroundColor;
-				const brightness = calculateBrightness(canvasBackgroundColor);
-				const threshold = 128; // Adjust as needed
-
-				if (brightness < threshold) {
-					this.color = "white";
-				} else {
-					this.color = "black";
+		
+			getRandomColor() {
+				const randomIndex = Math.floor(Math.random() * this.colors.length);
+				return this.colors[randomIndex];
+			}
+		
+			move() {
+				this.x += this.dx;
+				this.y -= this.dy;
+		
+				// Check if the particle is completely off the screen
+				if (
+					this.x + this.radius < 0 ||
+					this.x - this.radius > canvas.width ||
+					this.y + this.radius < 0 ||
+					this.y - this.radius > canvas.height
+				) {
+					// Add the particle to the removal array
+					particlesToRemove.push(this);
 				}
 			}
-
-			draw() {
+		
+      draw() {
 				const gradient = context.createRadialGradient(
 					this.x,
 					this.y,
@@ -42,81 +60,91 @@ const Bubbles = () => {
 					this.y + 0.5,
 					this.radius
 				);
-				gradient.addColorStop(0, "rgba(255, 255, 255, 0.8)");
-				gradient.addColorStop(1, "rgba(255, 255, 255, 0.2)");
+			
+				// Set gradient colors based on the random color
+				gradient.addColorStop(0, `rgba(${this.color}, 1)`);
+				gradient.addColorStop(0.5, `rgba(${this.color}, 0.8)`);
+				gradient.addColorStop(1, `rgba(${this.color}, 0.01)`);
+			
 				context.fillStyle = gradient;
-				context.shadowBlur = 10;
-				context.shadowColor = "rgba(255, 255, 255, 0.5)";
-
+				context.shadowBlur = 1;
+				// context.shadowColor = `rgba(${this.color}, 1)`;
+			
 				context.beginPath();
 				context.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
-				context.lineWidth = 0.2;
-				context.strokeStyle = this.color;
-				context.stroke();
+			
+				// Remove the outline color setting
+				// context.strokeStyle = "black"; // or context.strokeStyle = "white";
+			
+				context.lineWidth = 1;
+				// context.stroke(); // Remove this line to remove the outline
 				context.fill();
 			}
+			
+    }
 
-			move() {
-				this.x = this.x + this.dx;
-				this.y = this.y - this.dy;
-			}
-		};
+    const handleDrawCircle = (event) => {
+      const x = event.pageX;
+      const y = event.pageY;
 
-		const handleDrawCircle = (event) => {
-			const x = event.pageX;
-			const y = event.pageY;
+      for (let i = 0; i < 50; i++) {
+        const particle = new Particle(x, y);
+        particleArray.push(particle);
+      }
+    };
 
-			for (let i = 0; i < 50; i++) {
-				const particle = new Particle(x, y);
-				particleArray.push(particle);
-			}
-		};
+    const animate = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
 
-		const animate = () => {
-			context.clearRect(0, 0, canvas.width, canvas.height);
+      particleArray.forEach((particle) => {
+        particle?.move();
+        particle?.draw();
+      });
 
-			particleArray.forEach((particle) => {
-				particle?.move();
-				particle?.draw();
-			});
+      requestAnimationFrame(animate);
+    };
 
-			requestAnimationFrame(animate);
-		};
+    animate();
 
-		animate();
+    canvas.addEventListener("click", handleDrawCircle);
 
-		canvas.addEventListener("click", handleDrawCircle);
+    // Function to set the bubble color based on the background
+    const updateBubbleColor = () => {
+      for (const particle of particleArray) {
+        particle.color = particle.getRandomColor();
+      }
+    };
 
-		// Function to calculate brightness based on RGB values
-		const calculateBrightness = (color) => {
-			const rgb = color.match(/\d+/g).map(Number);
-			return (299 * rgb[0] + 587 * rgb[1] + 114 * rgb[2]) / 1000;
-		};
+    // Listen for changes to the dark mode class
+    const darkModeObserver = new MutationObserver(updateBubbleColor);
+    darkModeObserver.observe(document.body, {
+      attributes: true,
+      attributeFilter: ["class"],
+    });
 
-		// Function to set the bubble color based on the background
-		const updateBubbleColor = () => {
-			for (const particle of particleArray) {
-				particle.setColorBasedOnBackground();
-			}
-		};
+    // Initial call to set the bubble color
+    updateBubbleColor();
 
-		// Listen for changes to the dark mode class
-		const darkModeObserver = new MutationObserver(updateBubbleColor);
-		darkModeObserver.observe(document.body, {
-			attributes: true,
-			attributeFilter: ["class"],
-		});
+    // Remove particles that are off-screen
+    const removeOffScreenParticles = () => {
+      particlesToRemove.forEach((particle) => {
+        const index = particleArray.indexOf(particle);
+        if (index > -1) {
+          particleArray.splice(index, 1);
+        }
+      });
+      particlesToRemove.length = 0; // Clear the removal array
+    };
 
-		// Initial call to set the bubble color
-		updateBubbleColor();
-	}, []);
+    setInterval(removeOffScreenParticles, 6000); // Remove off-screen particles every 6 seconds
+  }, []);
 
-	return (
-		<canvas
-			ref={canvasRef}
-			style={{ background: "inherit", position: "absolute", top: 0, left: 0 }}
-		></canvas>
-	);
+  return (
+    <canvas
+      ref={canvasRef}
+      style={{ background: "inherit", position: "absolute", top: 0, left: 0 }}
+    ></canvas>
+  );
 };
 
 export default Bubbles;
