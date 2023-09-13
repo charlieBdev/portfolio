5282a7ef26ef39507217d20f802060cd982e7502
charlieBdev <chazbish@hotmail.com>
2023-09-13 15:06:08 +0100

    streamline code for iconsList

--- src/app/Components/IconCard.tsx
+++ src/app/Components/IconCard.tsx
@@ -6,7 +6,7 @@ interface IconCardProps {
 const IconCard: React.FC<IconCardProps> = ({ name, svg }) => {
 	return (
 		<div
-			className="text-3xl md:text-4xl lg:text-5xl p-3 rounded-lg tooltip tooltip-top"
+			className="text-3xl md:text-4xl lg:text-5xl tooltip tooltip-top"
 			data-tip={name}
 		>
 			<i className={svg} aria-label={name}></i>
--- src/app/Components/IconsList.tsx
+++ src/app/Components/IconsList.tsx
@@ -1,18 +1,16 @@
-"use client"
+"use client";
 
-import icons from "@/data/iconsArr"
-import IconCard from "./IconCard"
-
+import icons from "@/data/iconsArr";
+import IconCard from "./IconCard";
 
 const IconsList = () => {
-  
-  return (
-    <div className="rounded-lg flex flex-wrap justify-center mt-3 shadow dark:border dark:border-neutral-800">
-        {icons.map(icon => (
-          <IconCard key={icon.name} name={icon.name} svg={icon.svg} />
-        ))} 
-    </div>
-  )
-}
+	return (
+		<div className="p-3 gap-3 lg:gap-6 rounded-lg flex flex-wrap justify-center mt-3 shadow dark:border dark:border-neutral-800">
+			{icons.map((icon) => (
+				<IconCard key={icon.name} name={icon.name} svg={icon.svg} />
+			))}
+		</div>
+	);
+};
 
-export default IconsList
+export default IconsList;
