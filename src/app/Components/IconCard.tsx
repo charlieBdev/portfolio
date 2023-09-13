076559905172db09de74d6d546a1ba99c00f3c53
charlieBdev <chazbish@hotmail.com>
2023-09-13 13:05:20 +0100

    change icon size

--- src/app/Components/IconCard.tsx
+++ src/app/Components/IconCard.tsx
@@ -6,7 +6,7 @@ interface IconCardProps {
 const IconCard: React.FC<IconCardProps> = ({ name, svg }) => {
 	return (
 		<div
-			className="text-4xl md:text-5xl lg:text-6xl p-3 rounded-lg tooltip tooltip-top"
+			className="md:text-5xl lg:text-6xl p-3 rounded-lg tooltip tooltip-top"
 			data-tip={name}
 		>
 			<i className={svg} aria-label={name}></i>
