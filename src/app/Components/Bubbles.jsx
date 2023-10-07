e88c1708c29c4e6c5c0945930e04d9651e0e8acd
charlieBdev <chazbish@hotmail.com>
2023-10-07 18:16:12 +0100

    add sound

--- src/app/Components/Bubbles.jsx
+++ src/app/Components/Bubbles.jsx
@@ -2,8 +2,61 @@
 
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState, useEffect } from 'react';
+import * as Tone from 'tone';
+import { PingPongDelay, ToneAudioBuffer } from 'tone';
 
 export const Bubbles = () => {
+	useEffect(() => {
+		const buffer = new ToneAudioBuffer(
+			'https://tonejs.github.io/examples/audio/FWDL.mp3',
+			() => {
+				console.log('loaded');
+			}
+		);
+	}, []);
+
+	const [currentNoteIndex, setCurrentNoteIndex] = useState(0);
+	const synth = new Tone.MonoSynth().toDestination();
+
+	synth.volume.value = -20;
+
+	const pingPong = new PingPongDelay('4n', 0.2).toDestination();
+
+	synth.connect(pingPong);
+
+	const bubbleNotes = [
+		'A4',
+		'E4',
+		'F#4',
+		'A4',
+		'D5',
+		'E5',
+		'F#5',
+		'A5',
+		'F#5',
+		'E5',
+		'D5',
+		'A4',
+		'E4',
+		'F#4',
+		'A4',
+		'D5',
+		'E5',
+		'F#5',
+		'A5',
+		'F#5',
+		'E5',
+		'D5',
+		'A4',
+		'F#4',
+		'E4',
+		'F#4',
+		'A4',
+		'B4',
+		'C#5',
+		'A4',
+	];
+
 	const [bubbles, setBubbles] = useState([]);
 	const maxBubbleSize = 83;
 
@@ -45,6 +98,10 @@ export const Bubbles = () => {
 		// Filter out the bubble with the specified ID
 		const updatedBubbles = bubbles.filter((bubble) => bubble.id !== bubbleId);
 		setBubbles(updatedBubbles);
+
+		// Trigger the note on the synth
+		synth.triggerAttackRelease(bubbleNotes[currentNoteIndex], '8n');
+		setCurrentNoteIndex((prevIndex) => (prevIndex + 1) % bubbleNotes.length);
 	};
 
 	return (
