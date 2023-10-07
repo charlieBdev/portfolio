bc221d8b75f1ae1d39eb6acdb960ebada50c1d5f
charlieBdev <chazbish@hotmail.com>
2023-10-07 11:53:54 +0100

    add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { ThemeProvider } from './context/ThemeContext';
 import Navbar from './Components/Navbar';
 import Footer from './Components/Footer';
 import { Toaster } from 'sonner';
+// import Head from 'next/head';
+import Script from 'next/script';
 
 export const metadata: Metadata = {
 	title: 'Charlie B - Jnr Web Dev',
@@ -18,6 +20,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
+			{/* <Head> */}
 			<head>
 				<link
 					rel="stylesheet"
@@ -42,7 +45,17 @@ export default function RootLayout({
 					href="/favicon-16x16.png"
 				/>
 				<link rel="manifest" href="/site.webmanifest" />
+				{/* </Head> */}
 			</head>
+			<Script id="google-tag-manager" strategy="afterInteractive">
+				{`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${process.env.GOOGLE_ANALYTICS}');
+        `}
+			</Script>
 			<body className="flex flex-col min-h-screen justify-between">
 				<ThemeProvider>
 					<Navbar />
@@ -50,6 +63,11 @@ export default function RootLayout({
 					<Footer />
 					<Toaster richColors />
 				</ThemeProvider>
+				<noscript
+					dangerouslySetInnerHTML={{
+						__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_ANALYTICS}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+					}}
+				/>
 			</body>
 		</html>
 	);
