a47825d3b4dce0c703e53dcfc45546402c16bcab
charlieBdev <chazbish@hotmail.com>
2023-11-03 09:53:02 +0000

    fix header text alignment

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
 	return (
 		<header className=''>
 			<nav className='fixed top-0 left-0 right-0 px-6 h-20 xl:px-24 flex items-center justify-between shadow-xl bg-opacity-90 backdrop-blur dark:bg-opacity-90 dark:backdrop-blur z-10'>
-				<Link href='/' className='md:flex md:items-center md:space-x-3'>
+				<Link href='/' className='md:flex md:items-end md:space-x-3'>
 					<h1 className='text-2xl font-medium text-cyan-400'>Charlie Bishop</h1>
 					<p className='text-lg text-emerald-500'>
 						Junior
